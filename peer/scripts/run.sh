92a9f39c1e121a3c17bbdb3227d9b3344893174a
Enrique Pérez Arnaud <enrique@cazalla.net>
2015-06-25 15:12:51 +0200

    peer 0.18.0

--- peer/scripts/run.sh
+++ peer/scripts/run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+# Stop on error
+set -e
 
 if [[ -e /etc/firstrun ]]; then
     echo "Initializing database..."
-    django-admin.py syncdb --settings=peer.settings --noinput --all
-    django-admin.py migrate --settings=peer.settings --noinput --fake
+    django-admin.py migrate --settings=peer.settings --noinput
     echo "Creating admin user..."
     echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | \
           django-admin.py shell --settings=peer.settings
