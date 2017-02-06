67fbc5c1b7b5b194f61522d0f8e54eff0b7c6784
Enrique Pérez Arnaud <enrique@cazalla.net>
2017-02-06 10:08:16 +0100

    WIP

--- peer/config/local_db_settings.py
+++ peer/config/local_db_settings.py
@@ -1,8 +1,18 @@
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',      # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/data/peer.db',                     # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'peer',                                      # Or path to database file if using sqlite3.
+        'USER': 'peer',                                      # Not used with sqlite3.
+        'PASSWORD': 'peer',                                  # Not used with sqlite3.
+        'HOST': 'postgresql',                                          # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',                                          # Set to empty string for default. Not used with sqlite3.
     }
 }
+
+# DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',      # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        # 'NAME': '/data/peer.db',                     # Or path to database file if using sqlite3.
+    # }
+# }
