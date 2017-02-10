38dca7f9d3387703e608ee5cf6704830e76829da
Enrique Pérez Arnaud <enrique@cazalla.net>
2017-02-10 11:39:15 +0100

    useless outdated script

--- start-peer.sh
+++
@@ -1,6 +0,0 @@
-
-docker run --name=peer -p 7000:8000 \
-		--volume=prod-env/media:/usr/local/lib/python2.7/dist-packages/peer/media \
-		--volume=prod-env/static:/usr/local/lib/python2.7/dist-packages/peer/static \
-        --volume=prod-env/local_settings:/usr/local/lib/python2.7/dist-packages/peer/local_settings \
-		--volume=prod-env/data:/data peer
